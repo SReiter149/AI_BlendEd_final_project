7584134b720ba0315efd65672e853653548dc564
Hamuel101 <70246375+Hamuel101@users.noreply.github.com>
2022-12-01 17:44:39 -0800

    game resets on GAME_OVER

--- agent.py
+++ agent.py
@@ -1,23 +1,45 @@
 from layers_stable import Drop_Out_Layer, Activation_Layer, FC_Layer
 from network_stable import Network
 from functions_stable import pong_loss, sigmoid
+import numpy as np
+from numpy import random
+
 
 class agent:
-    def __init__(self, game, network):
+    def __init__(self, game):
         self.game = game
-        self.network1 = Network(pong_loss)
+        self.set_up()
 
-        self.network1.add(FC_Layer((6,24)))
-        self.network1.add(Activation_Layer())
-        self.network2 = network(sigmoid)
+        self.move1 = False #down
+        self.move2 = True #up
+    def set_up(self):
+        self.agent1 = Network(pong_loss)
+        self.agent1.add(FC_Layer((6,24), bias_true=False))
+        self.agent1.add(Activation_Layer())
+        self.agent1.add(FC_Layer((24,1), bias_true=False))
+
+        self.agent2 = Network(pong_loss)
+        self.agent2.add(FC_Layer((6,24), bias_true=False))
+        self.agent2.add(Activation_Layer())
+        self.agent2.add(FC_Layer((24,1), bias_true=False))
+
+    def get_action(self, state1, state2):
+        state1 = np.array(state1, dtype = float)
+        prediction = self.agent1.forward_prop(state1)
+        self.move1 = bool(round(prediction[0]))
+        
+        state2 = np.array(state1, dtype = float)
+        prediction = self.agent2.forward_prop(state2)
+        self.move2 = bool(round(prediction[0]))
 
     def main(self):
         game_over, state1, state2 = self.game.get_state()
-        while not game_over:
-            move1 = self.network1(state1)
-            move2 = self.network2(state2)
-            self.game.machine_play_frame(move1, move2)  # will update the game based on the move
+        while True:
+            self.get_action(state1, state2)
+            self.game.machine_play_frame(self.move1, self.move2)  # will update the game based on the move
             game_over, state1, state2 = self.game.get_state() # gets the new state based on the move
-            self.network1.update(state1)  # should update based on the reward
-            self.network2.update(state2)
-              
+            # self.agent1.back_prop(state1)   should update based on the reward
+            # self.agent2.back_prop(state2)
+            if game_over:
+                self.game.reset()
+
--- pong.py
+++ pong.py
@@ -142,7 +142,7 @@ class game:
         frame_time = time.time_ns() - start
         time.sleep((max(self.FPS - frame_time, 0)) * 10**-9)        
 
-    def machine_play_frame(self, p1_move, p2_move):
+    def machine_play_frame(self, p1_move, p2_move, view = True):
         #True = move up
         #False = move down
         start = time.time_ns()
@@ -168,30 +168,45 @@ class game:
         if self.ball.check_collision_paddle(self.paddle2):
             self.scores[1] += 1
         self.ball.move()
-        score1_image = self.FONT.render(str(self.scores[0]), True, self.WHITE)
-        score2_image = self.FONT.render(str(self.scores[1]), True, self.WHITE)
+        if view:
+            score1_image = self.FONT.render(str(self.scores[0]), True, self.WHITE)
+            score2_image = self.FONT.render(str(self.scores[1]), True, self.WHITE)
 
-        self.screen.fill(self.BLACK)
-        self.screen.blit(score1_image, (20, 20))
-        self.screen.blit(score2_image, (self.SCREEN_WIDTH - 20, 20))
-        self.ball.draw(self.WHITE)
-        self.paddle1.draw(self.WHITE)
-        self.paddle2.draw(self.WHITE)
-        pygame.display.flip()
+            self.screen.fill(self.BLACK)
+            self.screen.blit(score1_image, (20, 20))
+            self.screen.blit(score2_image, (self.SCREEN_WIDTH - 20, 20))
+            self.ball.draw(self.WHITE)
+            self.paddle1.draw(self.WHITE)
+            self.paddle2.draw(self.WHITE)
+            pygame.display.flip()
 
-        frame_time = time.time_ns() - start
-        time.sleep((max(self.FPS - frame_time, 0)) * 10**-9)   
-
+            frame_time = time.time_ns() - start
+            time.sleep((max(self.FPS - frame_time, 0)) * 10**-9)   
+    def get_state(self):
         #please check these because there is a good chance I made a mistake
         #I want these numbers to be the same if the positions are the same but flipped for each paddle
         state1 = [self.paddle1.position[0], self.paddle1.position[1], self.ball.position[0], self.ball.position[1], self.ball.direction[0], self.ball.direction[1]]
         state2 = [self.paddle1.position[0], self.paddle1.position[1], self.SCREEN_WIDTH - self.ball.position[0], self.SCREEN_HEIGHT - self.ball.position[1], -1 * self.ball.direction[0], self.ball.direction[1]]      
         return self.GAME_OVER, state1, state2
     
+    def reset(self):
+        self.GAME_OVER = False
+        self.BALL_START = [self.SCREEN_WIDTH / 2, self.SCREEN_HEIGHT / 2]
+        self.BALL_SIZE = 7
+        self.ball = Ball(self.screen, start_position=self.BALL_START, size=self.BALL_SIZE)
+        self.paddle1 = Paddle(self.screen, start_position=[self.PADDLE_OFFSET, 0], paddle_size=self.PADDLE_SIZE)
+        self.paddle2 = Paddle(
+            self.screen,
+            start_position=[self.SCREEN_WIDTH - self.PADDLE_OFFSET, 0],
+            paddle_size=self.PADDLE_SIZE,
+        )
+        self.scores = [0, 0]  # player1, then player2
 def main():
     pong = game()
-    while not pong.GAME_OVER:
-        pong.human_play_frame()
+    while True:
+        while not pong.GAME_OVER:
+            pong.human_play_frame()
+        pong.reset()
 
 
 
